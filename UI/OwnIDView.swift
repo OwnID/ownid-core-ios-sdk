70b1b1fab3b206f32612f80c0b558b1ee144719b
Yurii Boiko <Yurssoft@users.noreply.github.com>
2022-08-02 15:46:28 +0200

    Update OwnIDView.swift

--- UI/OwnIDView.swift
+++ UI/OwnIDView.swift
@@ -27,28 +27,58 @@ public extension OwnID.UISDK {
             self.isOrViewEnabled = visualConfig.isOrViewEnabled
         }
         
+        @State private var textOffsetFromScreenSide: CGFloat = 0
+        
         public var body: some View {
             HStack(spacing: 8) {
                 if isOrViewEnabled {
                     OwnID.UISDK.OrView()
                 }
-                GeometryReader { geometryReader in
                     ZStack {
                         imageButtonView
                             .layoutPriority(1)
                             .coordinateSpace(name: coordinateSpaceName)
-                        
-                        Text("*")
+                        Text("⬇️")
                             .fixedSize()
                             .background(.green)
-                            .offset(x: 0, y: -geometryReader.frame(in: .local).maxY)
-                        Text("Login with FaceID / TouchID ede fe")
+                            .offset(x: 0, y: -50)
+                        Text("Login with FaceID / TouchID start of the best")
                             .fixedSize()
                             .background(.red)
-                            .offset(x: -100, y: -80)
+                            .offset(x: textOffsetFromScreenSide, y: 0)
+                            .background(BackgroundGeometry())
+                            .onPreferenceChange(GeometryFramePreferenceKey.self, perform: { textOffsetFromScreenSide = calculateSpacingDistance(viewFrame: $0) })
                     }
-                }
             }
+        }
+        
+        private func calculateSpacingDistance(viewFrame: CGRect) -> CGFloat {
+            if viewFrame.maxX >= UIScreen.main.bounds.size.width {
+                let spacingToScreenSide: CGFloat = 10
+                let offsetFromScreenSide = UIScreen.main.bounds.size.width - viewFrame.maxX
+                return offsetFromScreenSide - spacingToScreenSide
+            }
+            return 0
+        }
+    }
+}
+
+struct GeometryFramePreferenceKey: PreferenceKey {
+    static var defaultValue = CGRect()
+
+    static func reduce(value: inout CGRect, nextValue: () -> CGRect) {
+        value = nextValue()
+    }
+
+    typealias Value = CGRect
+}
+
+struct BackgroundGeometry: View {
+    var body: some View {
+        GeometryReader { geometry in
+            Rectangle()
+                .fill(.clear)
+                .preference(key: GeometryFramePreferenceKey.self, value: geometry.frame(in: .global))
         }
     }
 }
