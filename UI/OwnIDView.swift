83fb307eaebfc4d0e93c2eaee1384462789e6b68
Yurii Boiko <Yurssoft@users.noreply.github.com>
2022-08-29 06:21:49 +0200

    Update OwnIDView.swift

--- UI/OwnIDView.swift
+++ UI/OwnIDView.swift
@@ -2,6 +2,11 @@ import Foundation
 import SwiftUI
 import OwnIDCoreSDK
 
+
+/// Possible future improvements:
+/// - default margin around tooltip can be calculated as function of the text size to have small margins when text is small and bigger when text is bigger
+/// support font as setting instead/along line height and text size to support changes with system sizes (maybe use .preferredFont(forTextStyle: ) ?)
+
 public extension OwnID.UISDK {
     struct OwnIDView: View {
         static func == (lhs: OwnID.UISDK.OwnIDView, rhs: OwnID.UISDK.OwnIDView) -> Bool {
