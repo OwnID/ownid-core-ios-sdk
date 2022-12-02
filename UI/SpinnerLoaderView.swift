ea5b3d82cd84aa6ae38770952f726d02a87d185d
Yurii Boiko <Yurssoft@users.noreply.github.com>
2022-12-02 15:02:06 +0100

    Divide view to functions

--- UI/SpinnerLoaderView.swift
+++ UI/SpinnerLoaderView.swift
@@ -8,34 +8,56 @@ extension OwnID.UISDK {
         }
         private let id = UUID()
         
-        private let style = StrokeStyle(lineWidth: 6, lineCap: .round, lineJoin: .round)
+        private let lineStyle = StrokeStyle(lineWidth: 6, lineCap: .round, lineJoin: .round)
         @State private var progress = 0.0
+        
+        private enum AnimationSteps: CaseIterable {
+            case inflate //from circle to 1/3
+            case deflate //from 1/3 to circle
+        }
+        
+        private let steps = [AnimationSteps.allCases]
         
         private var repeatingAnimation: Animation {
             Animation
-                .linear(duration: 10)
+                .linear(duration: 2)
                 .repeatForever(autoreverses: false)
         }
         
         var body: some View {
             ZStack {
-                Circle()
-                    .stroke(style: style)
-                    .foregroundColor(OwnID.Colors.spinnerBackgroundColor)
-                Circle()
-                    .trim(from: 0, to: progress)
-                    .stroke(style: style)
-                    .foregroundColor(OwnID.Colors.spinnerColor)
-                    .rotationEffect(Angle(degrees: -90))
-                    .rotationEffect(Angle(degrees: 360 * progress))
-                Circle()
-                    .trim(from: 0, to: 0.088)
-                    .stroke(style: style)
-                    .foregroundColor(OwnID.Colors.spinnerColor)
-                    .rotationEffect(Angle(degrees: -90))
-                    .rotationEffect(Angle(degrees: 360 * progress))
-                    .onAppear { withAnimation(repeatingAnimation) { progress = 1 } }
+                backgroundCircle()
+                staticCircle()
+                growingPartCircleLine()
             }
+            .onAppear { withAnimation(repeatingAnimation) { progress = 1 } }
+        }
+        
+        @ViewBuilder
+        private func growingPartCircleLine() -> some View {
+            Circle()
+                .trim(from: 0, to: 0.0044)
+                .stroke(style: lineStyle)
+                .foregroundColor(OwnID.Colors.spinnerColor)
+                .rotationEffect(Angle(degrees: -90))
+                .rotationEffect(Angle(degrees: 360 * progress))
+        }
+        
+        @ViewBuilder
+        private func staticCircle() -> some View {
+            Circle()
+                .trim(from: 0, to: progress)
+                .stroke(style: lineStyle)
+                .foregroundColor(OwnID.Colors.spinnerColor)
+                .rotationEffect(Angle(degrees: -90))
+                .rotationEffect(Angle(degrees: 360 * progress))
+        }
+        
+        @ViewBuilder
+        private func backgroundCircle() -> some View {
+            Circle()
+                .stroke(style: lineStyle)
+                .foregroundColor(OwnID.Colors.spinnerBackgroundColor)
         }
     }
 }
