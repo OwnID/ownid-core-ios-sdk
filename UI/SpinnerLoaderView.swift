cf41af6835d62fac3e396bd41504e18a00d316f5
Yurii Boiko <Yurssoft@users.noreply.github.com>
2022-12-02 06:23:05 +0100

    Add circle in circle

--- UI/SpinnerLoaderView.swift
+++ UI/SpinnerLoaderView.swift
@@ -8,9 +8,18 @@ extension OwnID.UISDK {
         }
         private let id = UUID()
         
+        private let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+        private let style = StrokeStyle(lineWidth: 6, lineCap: .round)
+        @State private var progress: CGFloat = 0.0 //why CGFloat anyway?
+        
         var body: some View {
-            Circle()
-                .strokeBorder(OwnID.Colors.spinnerBackgroundColor, lineWidth: 6)
+            ZStack {
+                Circle()
+                    .strokeBorder(OwnID.Colors.spinnerBackgroundColor, lineWidth: 6)
+                Circle()
+                    .trim(from: 0, to: 0.055)
+                    .stroke(OwnID.Colors.spinnerColor, lineWidth: 6)
+            }
         }
     }
 }
