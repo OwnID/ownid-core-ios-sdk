06bdee105c0f0a31552d9633e1d4097ef2cc21f5
Yurii Boiko <Yurssoft@users.noreply.github.com>
2022-12-02 07:22:25 +0100

    Add both circles to animation

--- UI/SpinnerLoaderView.swift
+++ UI/SpinnerLoaderView.swift
@@ -8,10 +8,14 @@ extension OwnID.UISDK {
         }
         private let id = UUID()
         
-        private let timer = Timer.publish(every: 0.05, on: .main, in: .common).autoconnect()
         private let style = StrokeStyle(lineWidth: 6, lineCap: .round, lineJoin: .round)
         @State private var progress = 0.0
-        @State private var isLoading = false
+        
+        private var repeatingAnimation: Animation {
+            Animation
+                .linear(duration: 10)
+                .repeatForever(autoreverses: false)
+        }
         
         var body: some View {
             ZStack {
@@ -19,18 +23,18 @@ extension OwnID.UISDK {
                     .stroke(style: style)
                     .foregroundColor(OwnID.Colors.spinnerBackgroundColor)
                 Circle()
-                    .trim(from: 0, to: 0.011)
+                    .trim(from: 0, to: progress)
                     .stroke(style: style)
                     .foregroundColor(OwnID.Colors.spinnerColor)
-                    .rotationEffect(Angle(degrees: isLoading ? 360 : 0))
-                    .animation(Animation.easeIn(duration: 2).repeatForever(autoreverses: false))
-                    .onAppear { isLoading.toggle() }
-                    .onReceive(timer) { _ in
-                        progress += 0.01
-                        if progress >= 1 {
-                            progress = 0
-                        }
-                    }
+                    .rotationEffect(Angle(degrees: -90))
+                    .rotationEffect(Angle(degrees: 360 * progress))
+                Circle()
+                    .trim(from: 0, to: 0.088)
+                    .stroke(style: style)
+                    .foregroundColor(OwnID.Colors.spinnerColor)
+                    .rotationEffect(Angle(degrees: -90))
+                    .rotationEffect(Angle(degrees: 360 * progress))
+                    .onAppear { withAnimation(repeatingAnimation) { progress = 1 } }
             }
         }
     }
