e0371d2c76a0bc07cc7f5d9c6ba05b06b6a2f9f7
Yurii Boiko <Yurssoft@users.noreply.github.com>
2022-12-02 07:03:51 +0100

    Simplify drawing animation

--- UI/SpinnerLoaderView.swift
+++ UI/SpinnerLoaderView.swift
@@ -9,18 +9,22 @@ extension OwnID.UISDK {
         private let id = UUID()
         
         private let timer = Timer.publish(every: 0.05, on: .main, in: .common).autoconnect()
-        private let style = StrokeStyle(lineWidth: 6, lineCap: .round)
+        private let style = StrokeStyle(lineWidth: 6, lineCap: .round, lineJoin: .round)
         @State private var progress = 0.0
+        @State private var isLoading = false
         
         var body: some View {
             ZStack {
                 Circle()
-                    .stroke(AngularGradient(colors: [OwnID.Colors.spinnerBackgroundColor], center: .center), style: style)
+                    .stroke(style: style)
+                    .foregroundColor(OwnID.Colors.spinnerBackgroundColor)
                 Circle()
                     .trim(from: 0, to: 0.011)
-                    .stroke(AngularGradient(colors: [OwnID.Colors.spinnerColor], center: .center), style: style)
-                    .rotationEffect(Angle.degrees(360 * progress))
-                    .animation(.default, value: progress)
+                    .stroke(style: style)
+                    .foregroundColor(OwnID.Colors.spinnerColor)
+                    .rotationEffect(Angle(degrees: isLoading ? 360 : 0))
+                    .animation(Animation.linear(duration: 2).repeatForever(autoreverses: false))
+                    .onAppear { isLoading.toggle() }
                     .onReceive(timer) { _ in
                         progress += 0.01
                         if progress >= 1 {
