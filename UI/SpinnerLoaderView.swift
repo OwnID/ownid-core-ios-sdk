117b2e5aa8fedba2a81bda724a75f7ea119d167b
Yurii Boiko <Yurssoft@users.noreply.github.com>
2022-12-05 20:19:21 +0100

    Decreasing progress

--- UI/SpinnerLoaderView.swift
+++ UI/SpinnerLoaderView.swift
@@ -11,76 +11,56 @@ extension OwnID.UISDK {
         private let lineStyle = StrokeStyle(lineWidth: 6, lineCap: .round, lineJoin: .round)
         private let spinnerColor = OwnID.Colors.spinnerColor
         private let startingTransformAngle = Angle(degrees: -90)
-        private let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
-        private let progressResetTimer = Timer.publish(every: 2, on: .main, in: .common).autoconnect()
-        @State private var progress = 0.0
+        @State private var increasingProgress = 0.0
+        @State private var decreasingProgress = 1.0
         
-        private enum AnimationSteps {
-            case inflate //from circle to 1/3
-            case deflate //from 1/3 to circle
-            
-            var inverted: Self {
-                switch self {
-                case .inflate:
-                    return .deflate
-                case .deflate:
-                    return .inflate
-                }
-            }
+        private var increasingAnimation: Animation {
+            Animation
+                .linear(duration: 10)
+                .repeatForever(autoreverses: false)
         }
         
-        @State private var step = AnimationSteps.inflate
-        
-        private var repeatingAnimation: Animation {
+        private var decreasingAnimation: Animation {
             Animation
-                .linear(duration: 1)
+                .linear(duration: 10)
+                .repeatForever(autoreverses: false)
+                .delay(1)
         }
         
         var body: some View {
             VStack {
                 ZStack {
                     backgroundCircle()
-                    staticCircle()
-                    growingPartCircleLine()
+                    decreasingCircle()
+//                    increasingCircle()
                 }
                 .frame(width: 200, height: 200)
-                Slider(value: $progress, in: 0...1)
-                Text("Percentage \(progress)")
-            }.onReceive(timer) { _ in
-                step = step.inverted
-                withAnimation(repeatingAnimation) {
-                    progress += 0.5
-//                    switch step {
-//                    case .inflate:
-//                        progress += 0.5
-//                    case .deflate:
-//                        <#code#>
-//                    }
-                }
-            }
-            .onReceive(progressResetTimer) { _ in
-                progress = 0
+                Slider(value: $increasingProgress, in: 0...1)
+                Text("Percentage \(increasingProgress)")
+            }.onAppear {
+                withAnimation(increasingAnimation) { increasingProgress = 1 }
+                withAnimation(decreasingAnimation) { decreasingProgress = 0 }
             }
         }
         
         @ViewBuilder
-        private func staticCircle() -> some View {
+        private func decreasingCircle() -> some View {
             Circle()
-                .trim(from: 0, to: 0.0044)
+                .trim(from: 0, to: decreasingProgress)
                 .stroke(style: lineStyle)
                 .foregroundColor(spinnerColor)
                 .rotationEffect(startingTransformAngle)
-                .rotationEffect(rotationAngle())
+                .rotationEffect(.degrees(-(360 * decreasingProgress * 2)))
         }
         
         @ViewBuilder
-        private func growingPartCircleLine() -> some View {
+        private func increasingCircle() -> some View {
             Circle()
-                .trim(from: 0, to: progress)
+                .trim(from: 0, to: increasingProgress)
                 .stroke(style: lineStyle)
                 .foregroundColor(spinnerColor)
                 .rotationEffect(startingTransformAngle)
-                .rotationEffect(rotationAngle())
+                .rotationEffect(.degrees(360 * increasingProgress * 2))
         }
         
         @ViewBuilder
@@ -89,7 +69,5 @@ extension OwnID.UISDK {
                 .stroke(style: lineStyle)
                 .foregroundColor(OwnID.Colors.spinnerBackgroundColor)
         }
-        
-        private func rotationAngle() -> Angle { Angle(degrees: 360 * progress * 2) }
     }
 }
