29d82a2dfe9f59b85a7e73f006e576dde6ec161a
Yurii Boiko <Yurssoft@users.noreply.github.com>
2022-12-15 06:50:15 +0100

    Fix correct passing of frame

--- UI/Tooltip/XAxisOffsetCalculating/ReactNativeLTRLayoutCalculation.swift
+++ UI/Tooltip/XAxisOffsetCalculating/ReactNativeLTRLayoutCalculation.swift
@@ -3,14 +3,14 @@ import SwiftUI
 extension OwnID.UISDK {
     struct ReactNativeLTRLayoutCalculation: XAxisOffsetCalculating {
         let isBottomPosition: Bool
-        let viewOrigin: CGPoint
+        let viewFrame: CGRect
         let textViewWidth: CGFloat
         
         func calculateXAxisOffset(viewBounds: CGRect, screenBounds: CGRect) -> CGFloat {
-            let startingPoint = Int(viewOrigin.x)
-            let widthAddedPoint = Int(viewOrigin.x + textViewWidth)
+            let startingPoint = Int(viewFrame.origin.x)
+            let widthAddedPoint = Int(viewFrame.origin.x + textViewWidth)
             for currentX in startingPoint...widthAddedPoint {
-                if !screenBounds.contains(.init(x: CGFloat(currentX), y: viewBounds.maxY)) {
+                if !screenBounds.contains(.init(x: CGFloat(currentX), y: viewFrame.maxY)) {
                     let offset = currentX - widthAddedPoint
                     return CGFloat(offset)
                 }
