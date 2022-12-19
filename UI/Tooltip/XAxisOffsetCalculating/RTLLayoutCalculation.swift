bb89cf7e0e1bf9e48bc38c6ed035f779cb610746
Yurii Boiko <Yurssoft@users.noreply.github.com>
2022-12-19 07:48:40 +0100

    Update RTLLayoutCalculation.swift

--- UI/Tooltip/XAxisOffsetCalculating/RTLLayoutCalculation.swift
+++ UI/Tooltip/XAxisOffsetCalculating/RTLLayoutCalculation.swift
@@ -7,13 +7,14 @@ extension OwnID.UISDK {
         let isNative: Bool
         
         func calculateXAxisOffset(viewBounds: CGRect, screenBounds: CGRect) -> CGFloat {
-            if isNative, isStartPosition {
-                let xOffset = viewFrame.width / 2
-                return xOffset
-            }
             if isStartPosition {
-                let xOffset = -(viewBounds.width - (viewFrame.width / 2))
-                return xOffset
+                if isNative {
+                    let xOffset = viewFrame.width / 2
+                    return xOffset
+                } else {
+                    let xOffset = -(viewBounds.width - (viewFrame.width / 2))
+                    return xOffset
+                }
             }
             let xOffset = viewBounds.origin.x + (viewFrame.width / 2) - viewBounds.width
             return xOffset
