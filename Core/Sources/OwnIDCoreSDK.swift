405560746b5c6096917f355a8255adc7c60ca0f1
Yurii Boiko <Yurssoft@users.noreply.github.com>
2023-01-16 08:49:57 +0100

    Bump version

--- Core/Sources/OwnIDCoreSDK.swift
+++ Core/Sources/OwnIDCoreSDK.swift
@@ -3,7 +3,7 @@ import Combine
 
 public extension OwnID.CoreSDK {
     static let sdkName = String(describing: OwnID.CoreSDK.self)
-    static let version = "2.1.1"
+    static let version = "2.2.0"
     static let APIVersion = "1"
 }
 
