c20ba7c3840ab136bf20c58d4da4a41664f92a50
Yurii Boiko <Yurssoft@users.noreply.github.com>
2023-02-01 08:41:54 +0100

    Create AuthButton.swift

---
+++ UI/AuthButton.swift
@@ -0,0 +1,12 @@
+import Foundation
+import SwiftUI
+
+extension OwnID.UISDK {
+    struct AuthButton: View {
+        var body: some View {
+            Button(action: { }) {
+                Image(systemName: "faceid")
+            }
+        }
+    }
+}
