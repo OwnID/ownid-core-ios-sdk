b0a448bff64ed4f7d297913de1e10070ebc31c46
Yurii Boiko <Yurssoft@users.noreply.github.com>
2023-03-29 16:54:48 -0500

    Update CoreViewModelTests.swift

--- Tests/CoreViewModelTests.swift
+++ Tests/CoreViewModelTests.swift
@@ -2,6 +2,22 @@ import XCTest
 import Combine
 @testable import OwnIDCoreSDK
 
+final class APISessionMock: APISessionProtocol {
+    var context: OwnID.CoreSDK.Context { "KreJ96smzSwveEb5QfaJzJ" }
+    
+    func performInitRequest(requestData: OwnID.CoreSDK.Init.RequestData) -> AnyPublisher<OwnID.CoreSDK.Init.Response, OwnID.CoreSDK.CoreErrorLogWrapper> {
+        Just().setFailureType(to: OwnID.CoreSDK.CoreErrorLogWrapper.self).eraseToAnyPublisher()
+    }
+    
+    func performFinalStatusRequest() -> AnyPublisher<OwnID.CoreSDK.Payload, OwnID.CoreSDK.CoreErrorLogWrapper> {
+        Just().setFailureType(to: OwnID.CoreSDK.CoreErrorLogWrapper.self).eraseToAnyPublisher()
+    }
+    
+    func performAuthRequest(fido2Payload: Encodable) -> AnyPublisher<OwnID.CoreSDK.Payload, OwnID.CoreSDK.CoreErrorLogWrapper> {
+        Just().setFailureType(to: OwnID.CoreSDK.CoreErrorLogWrapper.self).eraseToAnyPublisher()
+    }
+}
+
 final class CoreViewModelTests: XCTestCase {
     let sdkConfigurationName = OwnID.CoreSDK.sdkName
     var bag = Set<AnyCancellable>()
@@ -37,7 +53,7 @@ final class CoreViewModelTests: XCTestCase {
     func testSuccessRegistrationPathWithPasskeys() {
         let exp = expectation(description: #function)
         
-        let model = OwnID.CoreSDK.shared.createCoreViewModelForRegister(sdkConfigurationName: sdkConfigurationName)
+        let model = OwnID.CoreSDK.shared.createCoreViewModelForRegister(email: OwnID.CoreSDK.Email.init(rawValue: "lesot21279@duiter.com"), sdkConfigurationName: sdkConfigurationName)
         model.eventPublisher.sink { completion in
             switch completion {
             case .finished:
