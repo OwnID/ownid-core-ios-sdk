d4010cc3e791fbff1561f48ab26a6ee6c41c5d8a
Yurii Boiko <Yurssoft@users.noreply.github.com>
2023-03-30 07:07:48 -0500

    Update InstantConnectView.swift

--- UI/InstantConnectView.swift
+++ UI/InstantConnectView.swift
@@ -1,9 +1,45 @@
 import SwiftUI
+import UIKit
 import Combine
 
+public extension OwnID.UISDK.InstantConnectView {
+    static func displayInstantConnectView(emailPublisher: PassthroughSubject<String, Never>,
+                                          visualConfig: OwnID.UISDK.VisualLookConfig) {
+        func topMostController() -> UIViewController? {
+            guard let window = UIApplication.shared.keyWindow, let rootViewController = window.rootViewController else {
+                return nil
+            }
+
+            var topController = rootViewController
+
+            while let newTopController = topController.presentedViewController {
+                topController = newTopController
+            }
+
+            return topController
+        }
+        
+        let vc = topMostController()!
+        let v = UIView(frame: vc.view.frame)
+            v.backgroundColor = .red
+            v.layer.zPosition = CGFloat(Float.greatestFiniteMagnitude)
+        
+        vc.view.addSubview(v)
+        vc.view.bringSubviewToFront(v)
+        
+        let sv = OwnID.UISDK.InstantConnectView(emailPublisher: emailPublisher,
+                                                visualConfig: visualConfig)
+        let hosting = UIHostingController(rootView: sv)
+        hosting.willMove(toParent: vc)
+        v.addSubview(hosting.view)
+        hosting.view.frame = v.frame
+        v.bringSubviewToFront(hosting.view)
+        hosting.didMove(toParent: vc)
+    }
+}
+
 public extension OwnID.UISDK {
-    struct InstantConnectView<Content: View>: View {
-        private let content: () -> Content
+    struct InstantConnectView: View {
         private let emailPublisher: PassthroughSubject<String, Never>
         
         private let visualConfig: VisualLookConfig
@@ -13,10 +49,9 @@ public extension OwnID.UISDK {
         
         public init(emailPublisher: PassthroughSubject<String, Never>,
                     //                    viewState: Binding<ButtonState>,
-                    visualConfig: VisualLookConfig,
                     //                    isLoading: Binding<Bool>,
-                    @ViewBuilder content: @escaping () -> Content) {
-            self.content = content
+                    visualConfig: VisualLookConfig
+        ) {
             self.emailPublisher = emailPublisher
             _isLoading = .constant(false)
             _buttonState = .constant(.enabled)
@@ -34,39 +69,35 @@ public extension OwnID.UISDK {
         }
         
         public var body: some View {
-            if #available(iOS 15.0, *) {
-                content()
-                    .overlay(alignment: .bottom, content: {
-                        GeometryReader { geometry in
-                            VStack {
-                                HStack {
-                                    Text("Sign In")
-                                    Spacer()
-                                    Image("closeImage", bundle: .resourceBundle)
-                                }
-                                VStack {
-                                    Text("Enter your email")
-                                    TextField("", text: $email)
-                                        .background(Rectangle().fill(.white))
-                                        .padding()
-                                    AuthButton(visualConfig: visualConfig,
-                                               actionHandler: { print(geometry.frame(in: .local).height) },
-                                               isLoading: $isLoading,
-                                               buttonState: $buttonState)
-                                    .padding()
-                                }
-                            }
-                            .background(Rectangle().fill(.gray))
-                            .offset(x: -geometry.frame(in: .global).origin.x, y: -geometry.frame(in: .global).origin.y - (UIScreen.main.bounds.height - geometry.frame(in: .local).height))
-                        }
-                        .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
-                    })
-                    .onChange(of: email) { newValue in
-                        emailPublisher.send(newValue)
-                    }
+            if #available(iOS 14.0, *) {
+                viewContent()
+                    .onChange(of: email) { newValue in emailPublisher.send(newValue) }
             } else {
-                content()
+                viewContent()
             }
+        }
+        
+        @ViewBuilder
+        private func viewContent() -> some View {
+            VStack {
+                HStack {
+                    Text("Sign In")
+                    Spacer()
+                    Image("closeImage", bundle: .resourceBundle)
+                }
+                VStack {
+                    Text("Enter your email")
+                    TextField("", text: $email)
+                        .background(Rectangle().fill(.white))
+                        .padding()
+                    AuthButton(visualConfig: visualConfig,
+                               actionHandler: { resultPublisher.send(()) },
+                               isLoading: $isLoading,
+                               buttonState: $buttonState)
+                    .padding()
+                }
+            }
+            .background(Rectangle().fill(.gray))
         }
     }
 }
