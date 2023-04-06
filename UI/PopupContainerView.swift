cd74d5e4af8a19cdc9fab5af9d26d8fd11826a05
Yurii Boiko <Yurssoft@users.noreply.github.com>
2023-04-06 04:46:15 -0500

    Divide all into files for simplicity

--- UI/PopupContainerView.swift
+++ UI/PopupContainerView.swift
@@ -109,22 +109,3 @@ private extension OwnID.UISDK.PopupView {
             .animation(overlayAnimation, value: true)
     }
 }
-private extension OwnID.UISDK {
-    struct DeviceRotationViewModifier: ViewModifier {
-        let action: (UIDeviceOrientation) -> Void
-        
-        func body(content: Content) -> some View {
-            content
-                .onAppear()
-                .onReceive(NotificationCenter.default.publisher(for: UIDevice.orientationDidChangeNotification)) { _ in
-                    action(UIDevice.current.orientation)
-                }
-        }
-    }
-}
-
-extension View {
-    func onRotate(perform action: @escaping (UIDeviceOrientation) -> Void) -> some View {
-        self.modifier(OwnID.UISDK.DeviceRotationViewModifier(action: action))
-    }
-}
