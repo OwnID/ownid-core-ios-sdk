beca6dc4cbe9c5e88c3d66b6b56219a03a43c586
Yurii Boiko <Yurssoft@users.noreply.github.com>
2023-04-13 09:57:33 -0500

    Add otp text field

--- UI/OneTimePasswordView.swift
+++ UI/OneTimePasswordView.swift
@@ -69,6 +69,7 @@ extension OwnID.UISDK {
             if #available(iOS 15.0, *) {
                 return VStack {
                     topSection()
+                    OTPTextFieldView()
                     errorView()
                     TextButton(visualConfig: visualConfig,
                                actionHandler: { store.send(.codeEntered("1111")) },
@@ -144,6 +145,85 @@ extension OwnID.UISDK.OneTimePasswordView.Action: CustomDebugStringConvertible {
             return "cancel"
         case .emailIsNotRecieved:
             return "emailIsNotRecieved"
+        }
+    }
+}
+
+
+
+
+
+
+@available(iOS 15.0, *)
+public struct OTPTextFieldView: View {
+    enum FocusField: Hashable {
+        case field
+    }
+    @ObservedObject var viewModel = OTPViewModel()
+    @FocusState private var focusedField: FocusField?
+    private let codeLength = 6
+    private let boxSideSize: CGFloat = 50
+    private let spaceBetweenBoxes: CGFloat = 8
+    
+    private var backgroundTextField: some View {
+        return TextField("", text: $viewModel.verificationCode)
+            .frame(width: 0, height: 0, alignment: .center)
+            .font(Font.system(size: 0))
+            .accentColor(.clear)
+            .foregroundColor(.clear)
+            .multilineTextAlignment(.center)
+            .keyboardType(.numberPad)
+            .onReceive(Just(viewModel.verificationCode)) { _ in viewModel.limitText(codeLength) }
+            .focused($focusedField, equals: .field)
+            .task {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                    focusedField = .field
+                }
+            }
+            .padding()
+    }
+    
+    public var body: some View {
+        ZStack(alignment: .center) {
+            backgroundTextField
+            HStack(spacing: spaceBetweenBoxes) {
+                ForEach(0..<codeLength) { index in
+                    Text(viewModel.getPin(at: index))
+                        .font(Font.system(size: 27))
+                        .fontWeight(.semibold)
+                        .foregroundColor(.red)
+                        .background {
+                            Rectangle()
+                                .foregroundColor(.green)
+                        }
+                        .frame(width: boxSideSize, height: boxSideSize)
+                }
+            }
+        }
+    }
+}
+
+class OTPViewModel: ObservableObject {
+    var codeStorage = [
+        0: "",
+        1: "",
+        2: "",
+        3: "",
+        4: "",
+        5: ""
+    ]
+    @Published var verificationCode = ""
+    
+    func getPin(at index: Int) -> String {
+        guard verificationCode.count > index else {
+            return " "
+        }
+        return String(Array(verificationCode)[index])
+    }
+    
+    func limitText(_ upper: Int) {
+        if verificationCode.count > upper {
+            verificationCode = String(verificationCode.prefix(upper))
         }
     }
 }
