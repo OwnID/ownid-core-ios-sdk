8e3b8b9f8aa20656dde87527abd77f627b680ebd
Yurii Boiko <Yurssoft@users.noreply.github.com>
2023-04-20 16:03:35 -0500

    Update OrView.swift

--- UI/OrView.swift
+++ UI/OrView.swift
@@ -2,30 +2,27 @@ import SwiftUI
 
 extension OwnID.UISDK {
     struct OrView: View {
-        private let localizationChangedClosure: (() -> String)
-        @State private var translationText: String
+        @State private var isTranslationChanged = false
         let textSize: CGFloat
         let lineHeight: CGFloat
         let textColor: Color
         
         init(textSize: CGFloat, lineHeight: CGFloat, textColor: Color) {
-            let localizationChangedClosure = { OwnID.CoreSDK.TranslationsSDK.TranslationKey.or.localized() }
-            self.localizationChangedClosure = localizationChangedClosure
-            _translationText = State(initialValue: localizationChangedClosure())
             self.textSize = textSize
             self.lineHeight = lineHeight
             self.textColor = textColor
         }
         
         var body: some View {
-            Text(translationText)
+            Text(localizedKey: .or)
                 .fontWithLineHeight(font: .systemFont(ofSize: textSize), lineHeight: lineHeight)
                 .foregroundColor(textColor)
                 .multilineTextAlignment(.leading)
                 .fixedSize(horizontal: false, vertical: true)
                 .onReceive(OwnID.CoreSDK.shared.translationsModule.translationsChangePublisher) {
-                    translationText = localizationChangedClosure()
+                    isTranslationChanged.toggle()
                 }
+                .overlay(Text("\(String(isTranslationChanged))").foregroundColor(.clear), alignment: .bottom)
         }
     }
 }
