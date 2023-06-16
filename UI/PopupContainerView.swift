bbf89e4789e0c2bead9808f5a59c87bc720879d5
Oleksandr Maltsev <oleksandrmaltsev10@gmail.com>
2023-06-16 17:48:36 +0300

    Kyiv 2278 (#69)
    * Applied blur effect to views.
    
    * UI updates.
    
    ---------
    
    Co-authored-by: Oleksandr Maltsev <oleksandr@ownid.com>

--- UI/PopupContainerView.swift
+++ UI/PopupContainerView.swift
@@ -24,23 +24,34 @@ extension OwnID.UISDK {
 extension OwnID.UISDK {
     @available(iOS 15.0, *)
     struct PopupStackView: View {
+        private enum Constants {
+            static let contentCornerRadius: CGFloat = 10.0
+            static let animationResponse = 0.32
+            static let animationDampingFraction = 1.0
+            static let animationDuration = 0.32
+        }
+        
+        @Environment(\.colorScheme) var colorScheme
+        
         let popupContent: AnyPopup
         var body: some View {
             VStack(spacing: 0) {
                 Spacer()
                 ZStack(alignment: .bottom) {
                     popupContent.createContent()
-                        .background(OwnID.Colors.popupViewBackgroundColor
-                            .cornerRadius(9, corners: [.topLeft, .topRight])
-                            .ignoresSafeArea()
-                            .onTapGesture {
-                                //TODO: reimplement it using @FocusState
-                                UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
-                            })
+                        .background(colorScheme == .dark ? .regularMaterial : .thinMaterial)
+                        .containerShape(RoundedCorner(radius: Constants.contentCornerRadius, corners: [.topLeft, .topRight]))
+                        .onTapGesture {
+                            //TODO: reimplement it using @FocusState
+                            UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+                        }
                         .transition(.move(edge: .top))
                 }
             }
-            .animation(.spring(response: 0.32, dampingFraction: 1, blendDuration: 0.32), value: popupContent)
+            .animation(.spring(response: Constants.animationResponse,
+                               dampingFraction: Constants.animationDampingFraction,
+                               blendDuration: Constants.animationDuration),
+                       value: popupContent)
         }
     }
 }
--- UI/RoundCorners.swift
+++ UI/RoundCorners.swift
@@ -6,12 +6,19 @@ extension View {
     }
 }
 
-struct RoundedCorner: Shape {
+struct RoundedCorner: Shape, InsettableShape {
     var radius: CGFloat = .infinity
     var corners: UIRectCorner = .allCorners
+    var insetAmount: CGFloat = 0
 
     func path(in rect: CGRect) -> Path {
         let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
         return Path(path.cgPath)
+    }
+    
+    func inset(by amount: CGFloat) -> some InsettableShape {
+        var shape = self
+        shape.insetAmount += amount
+        return shape
     }
 }
